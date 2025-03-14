5679a5dffa13d06bddb94b48addc3ef7e0c90e8d
killsystem10 <killsystem10@outlook.com>
2025-03-14 10:54:58 +0900

    Part04_함수 사용하기

---
+++ function.js
@@ -0,0 +1,15 @@
+function return_test() {
+    return;
+    console.log("미실행 코드");
+}
+
+ function print(message) {
+    console.log("print function in");
+    console.log(message);
+    console.log("print function out");
+ }
+
+ function sum(arg1, arg2) {
+    var result = arg1 + arg2;
+    return result;
+ }
